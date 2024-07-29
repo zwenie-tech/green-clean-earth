54805b80a4fdce4d003cd1e0879d87beba3dfc44
Irshanac <irshana951@gmail.com>
2024-07-29 10:57:19 +0430

    add logout button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -46,7 +46,9 @@ const Dashboard=()=>{
           <h1 className='text-4xl font-bold'>Dashboard</h1>
         </div>
         <div className='ml-auto'>
-          <button className='rounded-xl bg-gray-200 p-2 md:mr-6' style={{boxShadow:'1px 4px 5px 3px #00000040'}}>Logout</button>
+          <button className='rounded-xl bg-gray-200 p-2 md:mr-6' 
+          onClick={LogoutButton}
+          style={{boxShadow:'1px 4px 5px 3px #00000040'}}>Logout</button>
         </div>
       </div>
       <div className='text-center'>
