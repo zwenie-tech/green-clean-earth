c6ee68995b6b99b78292c38f5e20d2a88a548922
Nihad <85496000+nihadgithub@users.noreply.github.com>
2024-09-19 17:30:08 +0530

    admin/users item preview and edit-form

---
+++ src/app/admin/users/[id]/edit-form.tsx
@@ -0,0 +1,420 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Edit } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { DateTimePicker } from "@/components/ui/dateTimePicker";
+const formSchema = z.object({
+  name: z.string().max(255),
+  email: z.string().email().min(1).max(255),
+  phoneNumber: z.coerce.number().gte(1).lte(255),
+  country: z.string().max(255),
+  state: z.string().max(255),
+  district: z.string().max(255),
+  corporation: z.string().max(255),
+  lsgd: z.string().max(255),
+  ward: z.string().min(1).max(255),
+  address: z.string().max(255),
+  gender: z.string(),
+  createdDate: z.date(),
+  city: z.string().max(255),
+  province: z.string().max(255),
+  coordinatorName: z.string().max(255),
+  groupName: z.string().max(255),
+});
+
+export function EditForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      // phoneNumber: ,
+      country: "",
+      state: "",
+      district: "",
+      corporation: "",
+      lsgd: "",
+      ward: "",
+      address: "",
+      city: "",
+      province: "",
+      coordinatorName: "",
+      groupName: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="flex items-center justify-start gap-2 my-4 cursor-pointer text-primary">
+          <Edit />
+          <span className="text-base">Edit</span>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-4xl overflow-y-scroll max-h-[98%]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div className="">
+          <Form {...form}>
+            <form
+              noValidate
+              onSubmit={form.handleSubmit(onSubmit)}
+              className=""
+            >
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input type="email" placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone number</FormLabel>
+                      <FormControl>
+                        <Input type="number" placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="country"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Country</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>State</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="district"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>District</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="corporation"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Corporation</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="lsgd"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Lsgd</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="ward"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Ward</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Address</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="gender"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Gender</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="m">Male</SelectItem>
+                          <SelectItem value="f">Female</SelectItem>
+                          <SelectItem value="n">Non-binary</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* <FormField
+                control={form.control}
+                name="createdDate"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Created date</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP")
+                            ) : (
+                              <span></span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          // disabled={(date) =>
+                          //   date > new Date() || date < new Date("1900-01-01")
+                          // }
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              /> */}
+
+                <FormField
+                  control={form.control}
+                  name="createdDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel>Created date</FormLabel>
+                      <FormControl>
+                        <DateTimePicker
+                          value={field.value}
+                          onChange={field.onChange}
+                          granularity="day"
+                          yearRange={30}
+                        />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>City</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="province"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Province</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="coordinatorName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Coordinator name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="groupName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Group Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormDescription></FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="mt-3">
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+        <DialogFooter>
+          {/* <Button type="submit">Save changes</Button> */}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,16 +1,101 @@
 "use client";
+import { ChevronLeft } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { EditForm } from "./edit-form";
 
 function Page() {
   const pathname = usePathname();
   const segments = pathname.split("/").filter(Boolean);
   const lastSegment = segments[segments.length - 1];
-  return <div className="text-4xl">
-    {lastSegment}
+  return (
+    <div className="">
+      {/* {lastSegment} */}
+      <div className="flex justify-between">
+        <div
+          className="flex items-center justify-start gap-2 my-4 cursor-pointer text-primary"
+          onClick={() => {
+            window.history.back();
+          }}
+        >
+          <ChevronLeft />
+          <span className="text-base">Manage users</span>
+        </div>
 
-    
-    </div>;
+        <EditForm />
+      </div>
+      <div className="grid gap-4 md:gap-6 grid-cols-1 md:grid-cols-3 p-2 md:p-5 md:border md:shadow-md md:rounded-lg">
+        <div className="">
+          <p className="text-sm text-gray-500">Name</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Email</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Phone number</p>
+          <p className="text-base">9876543210</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Country</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">State</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">District</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Corporation</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Lsgd</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Ward</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Address</p>
+          <p className="text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+            repellat consequuntur rerum et saepe perferendis dignissimos quam
+            soluta obcaecati
+          </p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Gender</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Created Date</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">City</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Province</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Coordinator Name</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+        <div className="">
+          <p className="text-sm text-gray-500">Group Name</p>
+          <p className="text-base">Lorem, ipsum dolor.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
